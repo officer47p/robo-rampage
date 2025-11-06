8653a5e38fa2006431627527cc43706b53c4e2f6
Parsa Hosseini <officer47p@gmail.com>
2025-11-06 12:29:57 +0330

    Add gun, recoil, print shooting, print raycast hit

---
+++ Weapons/hitscan_weapon.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+@export var fire_rate := 14.0
+@export var recoil := 0.05
+@export var weapon_mesh: Node3D
+
+@onready var cooldown_timer: Timer = $CooldownTimer
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+@onready var weapon_position: Vector3 = weapon_mesh.position
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("fire"):
+		if cooldown_timer.is_stopped():
+			shoot()
+			
+	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10)
+
+func shoot() -> void:
+	cooldown_timer.start(1.0/fire_rate)
+	printt("Firedd", ray_cast_3d.get_collider())
+	weapon_mesh.position.z += recoil
+	
